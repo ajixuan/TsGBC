d240b64ea31e9cdbcd5a21549f921b0d556ccce5
hkamran490 <hkamran490@gmail.com>
2016-12-10 16:50:59 -0500

    added ld opcodes

--- src/main/cpu/operations.ts
+++ src/main/cpu/operations.ts
@@ -7,13 +7,11 @@ import {Cpu} from "./cpu";
 export class Operations {
 
     private operations : Operation[];
-    public modes: Object;
-    public cpu : Cpu;
+    private cpu : Cpu;
 
     constructor(cpu : Cpu) {
         this.cpu = cpu;
-        this.createModes();
-        this.createOperations();
+        this.createMapping();
     }
 
     /**
@@ -26,15 +24,10 @@ export class Operations {
         return this.operations[opcode];
     }
 
-
-    private createModes() {
-
-    }
-
     /**
      * Create a list of opcodes with their respective addressing modes.
      */
-    private createOperations(): void {
+    private createMapping(): void {
         this.operations =  [];
 
         //System Pointers
@@ -57,29 +50,90 @@ export class Operations {
          */
 
         //----------------------------------------
-        // LD - Load (8 bits)
+        // LD nn,n - Load (8 bits)
         //----------------------------------------
 
-        this.operations[0x0E] = {
+        this.operations[0x06] = {
             name :"LD",
-            id : 0x0E,
             cycle : 8,
             mode : immediate,
-            execute(addr: number) : number {
+            size : 2,
+            execute(pc: number) {
+                var addr = this.mode.getValue(pc)
                 registers.setB(memory.readByte(addr));
-                return this.cycle;
             }
         };
 
-        this.operations[0x01] =  {
-            name : "LD",
-            id : 0x01,
+        this.operations[0x0E] = {
+            name :"LD",
             cycle : 8,
             mode : immediate,
-            execute(addr: number) : number {
-                return 0;
+            size : 2,
+            execute(pc: number) : number {
+                var addr = this.mode.getValue(pc)
+                registers.setC(memory.readByte(addr));
             }
-        }
+        };
+
+        this.operations[0x16] = {
+            name :"LD",
+            cycle : 8,
+            mode : immediate,
+            size : 2,
+            execute(pc: number) {
+                var addr = this.mode.getValue(pc)
+                registers.setD(memory.readByte(addr));
+            }
+        };
+
+        this.operations[0x1E] = {
+            name :"LD",
+            cycle : 8,
+            mode : immediate,
+            size : 2,
+            execute(pc: number) {
+                var addr = this.mode.getValue(pc)
+                registers.setE(memory.readByte(addr));
+            }
+        };
+
+        this.operations[0x26] = {
+            name :"LD",
+            cycle : 8,
+            mode : immediate,
+            size : 2,
+            execute(pc: number) {
+                var addr = this.mode.getValue(pc)
+                registers.setH(memory.readByte(addr));
+            }
+        };
+
+        this.operations[0x2E] = {
+            name :"LD",
+            cycle : 8,
+            mode : immediate,
+            size : 2,
+            execute(pc: number) {
+                var addr = this.mode.getValue(pc)
+                registers.setL(memory.readByte(addr));
+            }
+        };
+
+        //----------------------------------------
+        // LD nn,n - Load (8 bits)
+        //----------------------------------------
+
+        this.operations[0x7F] = {
+            name :"LD",
+            cycle : 4,
+            mode : immediate,
+            size : 2,
+            execute(pc: number) {
+                registers.setA(registers.getA());
+            }
+        };
+
+
 
         //----------------------------------------
         // LD - Load (16 bits)
@@ -96,8 +150,8 @@ interface Mode {
 
 interface Operation {
     name: string;
-    id: number;
     cycle: number;
     mode: Mode;
-    execute(addr: number) : number;
+    size: number;
+    execute(pc: number);
 }
