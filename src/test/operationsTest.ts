ecca5817aa7fc85f89b54747f71c7a6ab410ccfa
jixuan90 <xuan.ji90@gmail.com>
2017-01-07 01:10:00 -0500

    added first operations test

--- src/test/operationsTest.ts
+++ src/test/operationsTest.ts
@@ -2,11 +2,13 @@
  * Created by aji on 12/22/2016.
  */
 import {GameBoy} from '../main/gameboy';
-import assert = require('assert');
+import { suite, test, slow, timeout, skip, only} from "mocha-typescript";
 
+import assert = ts.Debug.assert;
 
 var gameboy = null;
-var operations = null;
+var cpu = null;
+var memory = null;
 
 try {
     gameboy = new GameBoy();
@@ -16,14 +18,37 @@ try {
     return;
 }
 
-operations = gameboy.cpu.operations;
+cpu= gameboy.cpu;
+memory = gameboy.memory;
 
 
-describe('Operations', function() {
-    describe('#carryFlags()', function() {
-        it('should set carry flags', function() {
-            operations.get(0x87);
-            assert.equal(, [1,2,3].indexOf(4));
+@suite("ALU 8Bit")
+class OperationsTest {
+
+    @test("0xC6 ALU ADD test")
+    testOutput() {
+        var a = memory.readByte(cpu.registers.getA());
+        var b = memory.readByte(cpu.registers.getB());
+        var result = a + b;
+        console.log(a); console.log(b);
+        cpu.operations.get(0xC6).execute(0);
+
+
+
+        //Check if flags are set
+        assert(cpu.registers.getSubtractFlag() == 0, "Subtract flag is not reset");
+        if()
+        assert(cpu.registers.getSubtractFlag() == 0, "Subtract flag is not reset");
+
+
+        //Check if the correct value is set
+        memory.readByte(cpu.registers.getA());
+
+    }
+
+    @test("should pass when promise resolved")
+    promise_pass_resolved() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(), 1);
         });
-    });
-});
+    }
