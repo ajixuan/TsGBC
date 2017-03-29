8a21650e8d104bec2cb68992f22f90d3bde9495a
Allen Ji <xuan.ji90@gmail.com>
2017-03-28 22:07:56 -0400

    reorganized memroy. moved all memory writes for ppu registers into ppu

--- src/main/memory/memory.ts
+++ src/main/memory/memory.ts
@@ -1,7 +1,6 @@
 import {Cartridge} from "../cartridge/cartridge";
 import {Interrupts} from "../io/interrupts";
-import {Registers} from "../ppu/registers"
-
+import {Ppu} from '../ppu/ppu'
 /**
  * Created by hkamran on 12/5/2016.
  */
@@ -9,15 +8,18 @@ import {Registers} from "../ppu/registers"
 
 export class Memory {
 
+    private bios:number[] =  [0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B,
+    0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D, 0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E,
+    0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC,
+    0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E];
+
     public io: number[] = [0x80];
     public cartridge: Cartridge;
     public interrupt: Interrupts = new Interrupts();
+    public ppu: Ppu = new Ppu(this);
 
-    public ppu: any = new class {
-        public vram: Array<number> = Array.apply(null, Array(0x2000)).map(Number.prototype.valueOf, 0);
-        public oam: Array<number> = Array.apply(null, Array(0xA0)).map(Number.prototype.valueOf, 0);
-        public registers: Registers = new Registers(this);
-    };
+    public vram: Array<number> = Array.apply(null, Array(0x1000)).map(Number.prototype.valueOf, 0);
+    public oam: Array<number> = Array.apply(null, Array(0xA0)).map(Number.prototype.valueOf, 0);
 
     public cpu: any = new class {
         public ram: Array<number> = Array.apply(null, Array(0x2000)).map(Number.prototype.valueOf, 0);
@@ -32,51 +34,22 @@ export class Memory {
         if (addr < 0x8000) {
             this.cartridge.writeByte(addr, val);
         } else if (addr < 0xA000) {
-            this.ppu.vram[addr - 0x8000] = val;
+            this.vram[addr - 0x8000] = val;
             this.ppu.updateTile(addr);
         } else if (addr < 0xC000) {
             this.cartridge.writeByte(addr, val);
         } else if (addr < 0xFE00) {
             this.cpu.ram[(addr - 0xC000) % 0x2000] = val;
         } else if (addr < 0xFEA0) {
-            this.ppu.oam[addr - 0xFE00] = val;
+            this.oam[addr - 0xFE00] = val;
         } else if (addr < 0xFF80) {
             if (addr < 0xFF00) {
                 throw "Invalid write on unused i/o at 0x" + addr.toString(16) + " with 0x" + val.toString(16);
             } else if (addr == 0xFF0F) {
                 this.interrupt.if = val & 0xFF;
-            }
-
-            //GPU registers
-            else if (addr == 0xFF40) {
-                this.ppu.registers.lcdc = val;
-            } else if (addr == 0xFF41) {
-                this.ppu.registers.stat = val;
-            } else if (addr == 0xFF42) {
-                this.ppu.registers.scy = val;
-            } else if (addr == 0xFF43) {
-                this.ppu.registers.scx = val;
-            } else if (addr == 0xFF44) {
-                this.ppu.registers.ly = val;
-            } else if (addr == 0xFF45) {
-                this.ppu.registers.lyc = val;
-            } else if (addr == 0xFF4A) {
-                this.ppu.registers.wy= val;
-            } else if (addr == 0xFF4B) {
-                this.ppu.registers.wx = val;
-            } else if (addr < 0xFF4C) {
-                this.io[addr - 0xFF00] = val;
-            } else if (addr == 0xFF68) {
-                this.ppu.registers.bcps = val;
-            } else if (addr == 0xFF69) {
-                this.ppu.registers.bcpd= val;
-            } else if (addr == 0xFF6A) {
-                this.ppu.registers.ocps = val;
-            } else if (addr == 0xFF6B) {
-                this.ppu.registers.ocpd = val;
-            }
-
-            else if (addr < 0xFF80) {
+            } else if (addr == 0xFF40) {
+                this.ppu.writeByte(addr, val);
+            } else if (addr < 0xFF80) {
                 throw "Invalid write on unused i/o at 0x" + addr.toString(16) + " with 0x" + val.toString(16);
             }
         } else if (addr < 0xFFFF) {
@@ -97,50 +70,23 @@ export class Memory {
         if (addr < 0x8000) {
             return this.cartridge.readByte(addr);
         } else if (addr < 0xA000) {
-            val = this.ppu.vram[addr - 0x8000];
+            val = this.vram[addr - 0x8000];
         } else if (addr < 0xC000) {
             return this.cartridge.readByte(addr);
         } else if (addr < 0xFE00) {
             val = this.cpu.ram[(addr - 0xC000) % 0x2000];
         } else if (addr < 0xFEA0) {
-            val = this.ppu.oam[addr - 0xFE00];
+            val = this.oam[addr - 0xFE00];
         } else if (addr < 0xFF80) {
             if (addr < 0xFF00) {
                 throw "Invalid read on unused i/o at 0x" + addr.toString(16);
             } else if (addr == 0xFF0F) {
                 return this.interrupt.if & 0xFF;
-            }
-
-            //GPU registers
-            else if (addr == 0xFF40) {
-                return this.ppu.registers.lcdc;
-            } else if (addr == 0xFF41) {
-                return this.ppu.registers.stat;
-            } else if (addr == 0xFF42) {
-                return this.ppu.registers.scy;
-            } else if (addr == 0xFF43) {
-                return this.ppu.registers.scx;
-            } else if (addr == 0xFF44) {
-                return this.ppu.registers.ly;
-            } else if (addr == 0xFF45) {
-                return this.ppu.registers.lyc;
-            } else if (addr == 0xFF4A) {
-                return this.ppu.registers.wy;
-            } else if (addr == 0xFF4B) {
-                return this.ppu.registers.wx;
             } else if (addr < 0xFF4C) {
                 val = this.io[addr - 0xFF00];
-            } else if (addr == 0xFF68) {
-                return this.ppu.registers.bcps;
-            } else if (addr == 0xFF69) {
-                return this.ppu.registers.bcpd;
-            } else if (addr == 0xFF6A) {
-                return this.ppu.registers.ocps;
-            } else if (addr == 0xFF6B) {
-                return this.ppu.registers.ocpd;
-            }
-
-            else if (addr < 0xFF80) {
+            } else if (addr < 0xFF6C) {
+                return this.ppu.readByte(addr);
+            } else if (addr < 0xFF80) {
                 throw "Invalid write on unused i/o at 0x" + addr.toString(16);
             }
         } else if (addr < 0xFFFF) {
--- src/main/ppu/ppu.ts
+++ src/main/ppu/ppu.ts
@@ -1,15 +1,27 @@
 import {Memory} from "../memory/memory";
 import {Screen} from "./screen";
+import {Registers} from "./registers";
 export class Ppu {
 
-    private screen : Screen;
-    private memory : Memory;
-    private tileset : Array<Array<Array<number>>>;
+    private screen: Screen;
+    private memory: Memory;
+    private tileset: Array<Array<Array<number>>>;
+    private registers: Registers;
 
+    //PPu units
+    private bgmapbase: number;
+    private bgtilebase: number;
+    private bgon: number;
+    private objon: number;
+    private objblock: number;
+    private winon: number;
+    private lcdon: number;
+    private winbase: number;
 
-    constructor(memory : Memory) {
+    constructor(memory: Memory) {
         this.screen = new Screen();
         this.memory = memory;
+        this.registers = Registers(this.memory);
         this.reset();
     }
 
@@ -17,43 +29,50 @@ export class Ppu {
      * reset()
      * Resets the bitmap
      */
-    public reset() : void {
+    public reset(): void {
         this.tileset = [];
 
         //Go through all tiles
-        for(var i = 0; i < 384; i++){
+        for (var i = 0; i < 384; i++) {
             this.tileset[i] = [];
 
             //For each tile there are 8 bits
-            for(var j = 0; j < 8; j++)
-            {
+            for (var j = 0; j < 8; j++) {
                 //Since each pixel consist 2 bytes, store values here
-                this.tileset[i][j] = [0,0,0,0,0,0,0,0];
+                this.tileset[i][j] = [0, 0, 0, 0, 0, 0, 0, 0];
             }
         }
     }
 
     /**
      * Write value to VRAM addr
+     * Updates one row of pixels
+     *
      * @param addr
      * @param val
      */
-    public updateTile(addr: number) : void {
+    public updateTile(addr: number): void {
 
         // Cut down the address so it is a number
         // that represents one of the 384 tiles
-        // The last address doesn't matter because
-        // each tile takes 2 bytes, so 2 addresses
+        // Addr is between 0x8000 - 0x97FF
+
+        // Last byte doesn't matter because each row is
+        // 2 bytes, so the second byte of the row will
+        // belong to the same row of bytes
         addr &= 0x1FFE;
 
-        //Each tile has 8 bytes. Divide the pixels by 8 (2 * 4 = 8)
+        // Each tile has 8 rows, each row takes 2 bytes. Divide the rows
+        // by 16 (shift 4 == divide 16)
+        // This will give us the tile number that the row belongs to
         var tile = (addr >> 4) & 0x1FF;
 
-        //We don't care about last address because each pixel takes up 2 addr
+        // Find the row number of the tile
         var y = (addr >> 1) & 0x7;
-
         var pindex;
-        for(var x = 0; x < 8; x++){
+
+        //8 bits
+        for (var x = 0; x < 8; x++) {
             pindex = 1 << x;
 
             this.tileset[tile][y][x] =
@@ -62,8 +81,141 @@ export class Ppu {
         }
     }
 
-    public renderscan(): void {
+
+    /**
+     * Return the address of the tile that is corresponding
+     * to the tile number
+     * @param index
+     */
+    private getTile(index: number): number {
+        //Check the lcdc for tile map
+
+        //Map 1
+        var mapaddr = 0x8000;
+
+        //Map 2
+        if(this.registers.lcdc & 0x10 == 0){
+            mapaddr = 0x8800;
+
+            //Convert value to negative
+            if(index & 0x80) {
+                index -= (1 << 8); //Sign extension trick
+            }
+        } else {
+
+
+
+        }
+
+
+
+
+        //Get tile
+        this.tileset
 
 
     }
+
+    /**
+     * Render scan on one tick
+     */
+    public renderscan(): void {
+
+        //Render bg data (0x3FF addresses)
+        for (var i = 0; i < 0x3FF; i++) {
+            //Char data
+            var data = this.readByte(this.bgmapbase + i);
+
+
+        }
+
+        this.registers.scy;
+        //Render obj
+
+    }
+
+    /**
+     * Gpu handles writing bytes to its own registers
+     * @param addr
+     * @param val
+     */
+    public writeByte(addr: number, val: number): void {
+        if (addr == 0xFF40) {
+            this.bgon = (val & 0x01) ? 1 : 0;                   //Bg Display
+            this.objon = (val & 0x02) ? 1 : 0;
+            this.objblock = (val & 0x04) ? 1 : 0;
+            this.bgmapbase = (val & 0x08) ? 0x9C00 : 0x9800;    //Two bg map locations
+            this.bgtilebase = (val & 0x10) ? 0x8000 : 0x8800;   //Two bg tile locations
+
+            this.winon = (val & 0x20) ? 1 : 0;
+            this.winbase = (val & 0x40) ? 0x9C00 : 0x9800;      //Window code area selection
+            this.lcdon = (val & 0x80) ? 1 : 0;                  //LCDC controller
+            this.registers.lcdc = val;
+        } else if (addr == 0xFF42) {
+            this.registers.scy = val;
+        } else if (addr == 0xFF43) {
+            this.registers.scx = val;
+        } else if (addr == 0xFF44) {
+            this.registers.ly = val;
+        } else if (addr == 0xFF45) {
+            this.registers.lyc = val;
+        } else if (addr == 0xFF46) {
+            //DMA
+            //Transfers 40 x 32 bits of data
+            //val is the starting address of transfer
+            //val is always the upper 8 bits, so need to be shifted
+            for (var i = 0; i < 0x9F; i++) {
+                var data = this.memory.readByte((val << 8) + i);
+                this.memory.oam[i] = data;
+            }
+        } else if (addr == 0xFF4A) {
+            this.registers.wy = val;
+        } else if (addr == 0xFF4B) {
+            this.registers.wx = val;
+        } else if (addr == 0xFF68) {
+            this.registers.bcps = val;
+        } else if (addr == 0xFF69) {
+            this.registers.bcpd = val;
+        } else if (addr == 0xFF6A) {
+            this.registers.ocps = val;
+        } else if (addr == 0xFF6B) {
+            this.registers.ocpd = val;
+        }
+    }
+
+    public readByte(addr: number): number {
+
+        if (addr < 0x9FFF) {
+            //Return color for pixel
+
+        }
+
+        switch (addr & 0xF000) {
+            case 0xFF40:
+                return this.registers.lcdc;
+            case 0xFF42:
+                return this.registers.scy;
+            case 0xFF43:
+                return this.registers.scx;
+            case 0xFF44:
+                return this.registers.ly;
+            case 0xFF45:
+                return this.registers.lyc;
+            case 0xFF4A:
+                return this.registers.wy;
+            case 0xFF4B:
+                return this.registers.wx;
+            case 0xFF68:
+                return this.registers.bcps;
+            case 0xFF69:
+                return this.registers.bcpd;
+            case 0xFF6A:
+                return this.registers.ocps;
+            case 0xFF6B:
+                return this.registers.ocpd;
+
+        }
+
+    }
+
 }
