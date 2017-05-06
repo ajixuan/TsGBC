0403830d039fbacc4330788d25e84c34f62c1f42
Allen Ji <xuan.ji90@gmail.com>
2017-05-06 15:17:40 -0400

    implemented scanline for one read of ly

--- gulpfile.js
+++ gulpfile.js
@@ -8,67 +8,67 @@ var mocha = require('gulp-mocha');
 var Builder = require('systemjs-builder');
 
 var dir = {
-  src: 		'./src/main/**/*.ts',
-  test:     './src/test/**/*.ts',
-  html: 	'./src/html/**/*',
-  target:	'./target/**/*'
+    src: './src/main/**/*.ts',
+    test: './src/test/**/*.ts',
+    html: './src/html/**/*',
+    target: './target/**/*'
 };
 
-    gulp.task('clean', function() {
-        return del(['target']);
-    });
-
-    gulp.task('test', function() {
-        return gulp.src(dir.test)
-            .pipe(ts({
-                outFile:'test.js',
-                removeComments: false,
-                preserveConstEnums: true,
-                sourceMap: true,
-                experimentalDecorators: true,
-                declaration: true,
-                module: 'system'
-            }))
-            .pipe(gulp.dest('target'))
-            .pipe(mocha());
+gulp.task('clean', function () {
+    return del(['target']);
 });
 
-gulp.task('src', function() {
-	return gulp.src(dir.src)
-		.pipe(ts({
+gulp.task('test', function () {
+    return gulp.src(dir.test)
+        .pipe(ts({
+            outFile: 'test.js',
+            removeComments: false,
+            preserveConstEnums: true,
+            sourceMap: true,
+            experimentalDecorators: true,
+            declaration: true,
+            module: 'system'
+        }))
+        .pipe(gulp.dest('target'))
+        .pipe(mocha());
+});
+
+gulp.task('src', function () {
+    return gulp.src(dir.src)
+        .pipe(ts({
             typescript: require('typescript'), // In my package.json I have "typescript": "~1.8.0-dev.20151128"
             module: 'system',
             experimentalDecorators: true,
             emitDecoratorMetadata: true,
-            outFile : 'app.js',
+            outFile: 'app.js',
             target: "es5"
         }))
         .pipe(gulp.dest('target'));
 });
 
-gulp.task('html', function() {
-	return gulp.src(dir.html)
-		.pipe(gulp.dest('target/'));
+gulp.task('html', function () {
+    return gulp.src(dir.html)
+        .pipe(gulp.dest('target/'));
 });
 
-gulp.task('package', function() {
-	var name = "RTCJS-" + p.version + ".tar";
-	return gulp.src(['!target/' + name, dir.target])
-		.pipe(tar(name))
-		.pipe(gulp.dest('target/'));
+gulp.task('package', function () {
+    var name = "RTCJS-" + p.version + ".tar";
+    return gulp.src(['!target/' + name, dir.target])
+        .pipe(tar(name))
+        .pipe(gulp.dest('target/'));
 });
 
-gulp.task('build', function(callback) {
+gulp.task('build', function (callback) {
     runSequence('clean', 'src', 'html', callback);
 });
 
-gulp.task('watch', function() {
-  gulp.watch([dir.src, dir.html], ['build']);
+gulp.task('watch', function () {
+    gulp.watch([dir.src, dir.html], ['build']);
 });
 
-gulp.task('default', function(callback) {
-	runSequence('clean',
-				'build',
-				'package',
-				 callback);
+gulp.task('default', function (callback) {
+    runSequence('clean',
+        'build',
+        'package',
+        callback);
 });
