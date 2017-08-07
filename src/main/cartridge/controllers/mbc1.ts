19ea0866c5747e6b9c7049ae2a2efe793172583e
Allen Ji <xuan.ji90@gmail.com>
2017-08-06 21:22:08 -0400

    Begining implementing mbc1

--- src/main/cartridge/controllers/mbc1.ts
+++ src/main/cartridge/controllers/mbc1.ts
@@ -5,40 +5,85 @@ import {Controller} from "./controller";
 export class MBC1 implements Controller {
 
     public rom : number[];
+    public ram : number[];
+    public eram : number;
+    public mode : number;
+    public rombank : number = 0;
+    public rambank : number = 0;
 
     constructor( rom : number[]) {
         this.rom = rom;
+        this.ram = Array(null, 0x2000 * 3).map(Number.prototype.valueOf, 0);
+        this.eram = 0;
     }
 
     public readByte(addr: number) : number {
 
-        if(addr < 0x2000){
-
-
-        } else if (addr < 0x4000) {
-            return this.rom[addr];
-        } else if (addr < 0x6000) {
-
-
-        } else if (addr < 8000){
-
-
-        } else if (addr >= 0xA000 && addr < 0xC000) {
-
-
-        }
-
-
-        throw "Invalid read at 0x" + addr.toString(16) + " at cartridge controller.";
-    }
-    public writeByte(addr: number, val : number) : void {
+        //Rom bank
         if (addr < 0x8000) {
-            this.rom[addr] = val;
-            return;
-        } else if (addr < 0xC000 && addr >= 0xA000) {
-            return;
+            this.rombank = 0;
+            return this.rom[addr  + (this.rombank * 0x4000)];
+        }
+        //Ram bank
+        else if (addr >= 0xA000 && addr < 0xC000) {
+            this.rambank = 0;
+            if(this.eram){
+                return this.ram[addr + (this.rambank * 0x2000)]
+            }
+            return 0xFF;
         }
 
-        throw "Invalid write at 0x" + addr.toString(16) + " at cartridge controller.";
+
+        throw "Invalid read at 0x" + addr.toString(16) + " in cartridge controller.";
+    }
+
+    public writeByte(addr: number, val : number) : void {
+        switch(addr & 0xF000){
+            case 0x0000:
+            case 0x1000:
+                if(val == 0x0A){
+                    this.eram = 1;
+                } else {
+                    this.eram = 0;
+                }
+                break;
+            case 0x2000:
+            case 0x3000:
+                //Rom bank low 5 bits
+                //Switch between banks 1 and 31
+                this.rombank &= 0x60;
+                switch(val){
+                    case 0x00:
+                    case 0x20:
+                    case 0x40:
+                    case 0x60:
+                        val++;
+                }
+                this.rombank = val;
+                break;
+            case 0x4000:
+            case 0x5000:
+                //Ram
+                if(this.mode){
+                    this.rambank = val;
+                    break;
+                }
+
+                if(val > 0x7F) {
+                    throw "ERROR: bank 0x" + val.toString(16) + " does not exist";
+                }
+                this.rombank |= (val << 5);
+                break;
+            case 0x6000:
+            case 0x7000:
+                //Rom mode
+                this.mode = val;
+                break;
+            case 0xA000:
+            case 0xB000:
+                this.eram
+                break;
+        }
+
     }
 }
